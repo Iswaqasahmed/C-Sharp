c15cfe14d54f73331c8664c211774a5cefab0ead
waqas ahmed <37307194+Iswaqasahmed@users.noreply.github.com>
2021-06-19 13:04:51 +0500

    add c-sharp basic file

---
+++ C# Basic/Differences between Variable Instance and Reference/Differences between Variable Instance Reference/Variable.Instance.Reference/First.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Variable.Instance.Reference
+{
+    class CVariable
+    {
+        // Class is user define data type: its blue print of data, it doesn't have memory allocation
+        // int i ;  memeory null
+
+        // int = 100; it is only plan means its blue print of class
+        // int i = 100 , its constuctor of class
+
+
+
+        static int var1;
+        static int var2;
+    }
+    class First
+    {
+        // non static member are access through instance of class
+        int x = 100;
+        static void Main(string[] args)
+        {
+            // First cVariable -----> null
+            // x = 100  ----------->  cVariable = new First();
+
+            /// Instance of class: a copy that is initialized the new keyword
+
+            First cVariable;  // variable of class
+            cVariable = new First();    // cvariable is instance of class
+            Console.WriteLine(cVariable.x); // accessing the instance of class 
+
+
+
+
+            /// reference of class: a class variable 
+            ///  
+            First first = new First(); // first instance of class
+            First second = new First(); // second instance of class
+            Console.WriteLine(first.x + " " + second.x);
+
+            first.x = 100;
+            Console.WriteLine(first.x + " " + second.x);
+
+            second.x = 200;
+            Console.WriteLine(first.x + " " + second.x);
+
+
+
+
+        }
+    }
+}
