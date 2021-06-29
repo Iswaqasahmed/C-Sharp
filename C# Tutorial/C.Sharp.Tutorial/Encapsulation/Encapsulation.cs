95715c907320307aafeaa2bc2fe03d81bb78cf4c
waqas ahmed <wa541964@gmail.com>
2021-06-29 23:37:26 +0500

    Add folder

---
+++ C# Tutorial/C.Sharp.Tutorial/Encapsulation/Encapsulation.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace C.Sharp.Tutorial.Encapsulation
+{
+    class Encapsulation
+    {
+        // Encapsulation is a mechanism of wrapping the data(variable) and code acting on the data
+        // (method or properties) together as a single unit.
+        public Encapsulation()
+        {
+            Console.WriteLine("Encapusulation Class ...");
+        }
+    }
+}
--- C# Tutorial/C.Sharp.Tutorial/Program.cs
+++ C# Tutorial/C.Sharp.Tutorial/Program.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using C.Sharp.Tutorial.Encapsulation;
+
 
 namespace C.Sharp.Tutorial
 {
@@ -10,6 +12,8 @@ namespace C.Sharp.Tutorial
     {
         static void Main(string[] args)
         {
+
+            Encapsulation.Encapsulation ec = new Encapsulation.Encapsulation();
 
             Console.WriteLine("Main in Program Class");
             
