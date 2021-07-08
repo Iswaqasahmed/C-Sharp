0738b211933a7c75949c5713ce6797deaa1bb4b1
waqas ahmed <wa541964@gmail.com>
2021-07-09 00:04:47 +0500

    add singleton class

--- C# Tutorial/C.Sharp.Tutorial/Complex.Class/Complex.cs
+++ C# Tutorial/C.Sharp.Tutorial/Complex.Class/Complex.cs
@@ -60,4 +60,37 @@ namespace C.Sharp.Tutorial.Complex.Class
         }
 
     }
+
+    class ComplexNumbers
+    {
+        public static  string Token()
+        {
+            string token = Convert.ToBase64String(Guid.NewGuid().ToByteArray());
+            byte[] time = BitConverter.GetBytes(DateTime.UtcNow.ToBinary());
+            byte[] key = Guid.NewGuid().ToByteArray();
+            return token;
+        }
+        static void Main(string[] args)
+        {
+            //Complex c1, c2, c3;
+            //c1 = new Complex();
+            //c1.SetData(2.0f,2.0f);
+            //c2 = new Complex();
+            //c3 = new Complex();
+            ////c3 = c1.AddComplex(c2);
+            //c3 = c1.AddComplex(new Complex());
+            //Console.WriteLine("Complex C3");
+            //c3.DisplayData();
+           
+            byte[] data = Convert.FromBase64String(Token());
+            DateTime when = DateTime.FromBinary(BitConverter.ToInt64(data, 0));
+            if (when < DateTime.UtcNow.AddHours(-24))
+            {
+                Console.WriteLine(" " + data.ToArray());
+            }
+            Console.WriteLine();
+        }
+    }
+
+
 }
---
+++ C# Tutorial/C.Sharp.Tutorial/Singleton.Design/SingletonClass.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C.Sharp.Tutorial.Singleton.Design
+{
+    class SingletonClass
+    {
+        public static SingletonClass singletonInstance; // why class reference variable  to 
+        /// <summary>
+        /// no one will create its instance
+        /// </summary>
+        private SingletonClass()
+        {
+
+        }
+
+        /// <summary>
+        /// To control only one instance of class 
+        /// </summary>
+        /// <returns>class instanfea</returns>
+        public static SingletonClass GetInstance()
+        {
+            if (singletonInstance == null)
+            {
+                singletonInstance = new SingletonClass();
+            }
+
+            return singletonInstance;
+        }
+
+
+
+
+    }
+}
