dc2894ec89e2b32b1aaf97840d21ed0255a089c9
waqas ahmed <wa541964@gmail.com>
2021-07-08 21:36:38 +0500

    add complex class method

--- C# Tutorial/C.Sharp.Tutorial/Complex.Class/ComplexClass.cs
+++ C# Tutorial/C.Sharp.Tutorial/Complex.Class/ComplexClass.cs
@@ -15,8 +15,43 @@ namespace C.Sharp.Tutorial.Complex.Class
 
         public ComplexClass(float r,float i)
         {
-
+            real = r;
+            image = i;
         }
+
+        public void GetData()
+        {
+            float r, i;
+            Console.WriteLine("Enter Real and Imaginary Part");
+            r = Single.Parse(Console.ReadLine());
+            i = Single.Parse(Console.ReadLine());
+            real = r;
+            image = i;
+        }
+
+        public void SetData(float r,float i)
+        {
+            real = r;
+            image = i;
+        }
+
+        public void DisplayData()
+        {
+            Console.WriteLine("Real {0} ",real);
+            Console.WriteLine("Imaginary = {0}",image);
+        }
+
+        // complex class that is somelogic
+        public ComplexClass AddComplex(ComplexClass y)
+        {
+            ComplexClass t;
+            t = new ComplexClass();
+            t.real = real + y.real;
+            t.image = image + y.image;
+            return t; // what would be t return ?
+        }
+
+
 
     }
 }
