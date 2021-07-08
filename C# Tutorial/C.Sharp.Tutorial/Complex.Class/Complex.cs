cd378668367de9f46433488ce89ff44714e15613
waqas ahmed <wa541964@gmail.com>
2021-07-09 00:04:47 +0500

    add singleton class

--- C# Tutorial/C.Sharp.Tutorial/Complex.Class/Complex.cs
+++ C# Tutorial/C.Sharp.Tutorial/Complex.Class/Complex.cs
@@ -8,7 +8,6 @@ namespace C.Sharp.Tutorial.Complex.Class
 {
     class Complex
     {
-        internal float? real = null, image = null;
         public Complex()
         {
         }
@@ -59,5 +58,39 @@ namespace C.Sharp.Tutorial.Complex.Class
             return t;
         }
 
+        internal float? real = null, image = null;
     }
+
+    class ComplexNumbers
+    {
+        public static  string Token()
+        {
+            string token = Convert.ToBase64String(Guid.NewGuid().ToByteArray());
+            byte[] time = BitConverter.GetBytes(DateTime.UtcNow.ToBinary());
+            byte[] key = Guid.NewGuid().ToByteArray();
+            return token;
+        }
+        static void Main(string[] args)
+        {
+            Complex c1, c2, c3;
+            c1 = new Complex();
+            c1.SetData(2.0f, 2.0f);
+            c2 = new Complex();
+            c3 = new Complex();
+            //c3 = c1.AddComplex(c2);
+            c3 = c1.AddComplex(new Complex()));
+            Console.WriteLine("Complex C3");
+            c3.DisplayData();
+
+            //byte[] data = Convert.FromBase64String(Token());
+            //DateTime when = DateTime.FromBinary(BitConverter.ToInt64(data, 0));
+            //if (when < DateTime.UtcNow.AddHours(-24))
+            //{
+            //    Console.WriteLine(" " + data.ToArray());
+            //}
+            //Console.WriteLine();
+        }
+    }
+
+
 }
