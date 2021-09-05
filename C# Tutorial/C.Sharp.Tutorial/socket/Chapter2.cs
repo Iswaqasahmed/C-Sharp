f848a795ddec9d2bd810c37399fb8e632662a123
waqas ahmed <wa541964@gmail.com>
2021-09-05 20:02:50 +0500

    new class added
    Chapter 2 class added in project.

---
+++ C# Tutorial/C.Sharp.Tutorial/socket/Chapter2.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C.Sharp.Tutorial.socket
+{
+    class Chapter2
+    {
+        static void main(string[] args)
+        {
+
+        }
+    }
+}
