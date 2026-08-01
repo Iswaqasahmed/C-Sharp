b262cf15eacd1397bd752b92246345cb86de6985
Waqas Ahmed <Waqas Ahmed@WAQASAHMED>
2026-08-02 02:35:16 +0500

    My Changed.

--- C# Tutorial/C.Sharp.Tutorial/Program.cs
+++ C# Tutorial/C.Sharp.Tutorial/Program.cs
@@ -1,10 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using C.Sharp.Tutorial.Encapsulation;
-using C.Sharp.Tutorial.socket;
+﻿using MailKit.Security;
+using MimeKit;
+using System;
 
 namespace C.Sharp.Tutorial
 {
@@ -12,6 +8,40 @@ namespace C.Sharp.Tutorial
     {
         static void Main(string[] args)
         {
+
+
+
+
+
+            var message = new MimeMessage();
+            message.From.Add(new MailboxAddress("Test", "manas@pk.gt.com"));
+            message.To.Add(new MailboxAddress("You", "your-email@example.com"));
+            message.Subject = "SMTP Test";
+            message.Body = new TextPart("plain")
+            {
+                Text = "SMTP test successful."
+            };
+
+             var client = new System.Net.Mail.SmtpClient();
+
+            try
+            {
+                client.Connect("eu-test.com", 587, SecureSocketOptions.StartTls);
+                client.Authenticate("manas@pk.gt.com", "YOUR_SMTP_PASSWORD");
+
+                client.Send(message);
+                client.Disconnect(true);
+
+                Console.WriteLine("Email sent successfully.");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+
+
+
+
 
             //Encapsulation.Encapsulation ec = new Encapsulation.Encapsulation();
 
